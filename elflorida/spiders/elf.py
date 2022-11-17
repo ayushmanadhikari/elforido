0399345c381d435cfd7f641c567e1bc3c67802ad
ayushmanadhikari <unixparadoxfb@gmail.com>
2022-11-17 22:46:49 +0545

    new

--- elflorida/spiders/elf.py
+++ elflorida/spiders/elf.py
@@ -31,12 +31,15 @@ class ElfSpider(scrapy.Spider):
 
                 category_url2 = "https://tienda.elflorido.com.mx:4433/Search/Products?search=&siteId=136&pageCount=40"+urlencode({'promos': 'false','categoryId': category_id, 'subcategoryId': sub_category_id})
 
+                
+
                 yield scrapy.Request(url=category_url2, callback=self.parse_product_list)
 
 
 
     def parse_product_list(self, response):
-        item = ElfloridaItem()
+
+
         raw_data = response.body
         data = json.loads(raw_data)
         data = data['data']
@@ -46,20 +49,21 @@ class ElfSpider(scrapy.Spider):
             product['Canal'] = ""
             product['Category'] = d['categoryName']
             product['Subcategory'] = d['subcategoryName']
-            
+
+
+            #gets sub categories
             subs_list = product['Subcategory'].split(",")
             count = 0
             for sub in subs_list:
                 count = count + 1
                 col1 = f"Subcategory{count}"
-                product[col1] = sub
-                print(count)           
-
+                product[col1] = sub           
             del product['Subcategory']
+
 
             product['Marca'] = ""
             product['Modelo'] = ""
-            product['SKU'] = ""
+            product['SKU'] = d['slug']
             product['UPC'] = d['slug']
             product['Item'] = d['productName']
             product['Item Characteristics'] = d['productDescription']
@@ -74,12 +78,40 @@ class ElfSpider(scrapy.Spider):
             product['Store Address'] = ""
             product['Stock'] = d['stock']
             product['UPC WM'] = ""
-            product['Final Price'] = ""
+            product['Final Price'] = d['priceTotal']
+            product['productId'] = d['productId']
+        
 
-            yield product
             
+            yield product
+
+            # https://tienda.elflorido.com.mx:4433/Search/GetVariants?productId=8171&siteId=136
+
+            # sale_flag_url = "https://tienda.elflorido.com.mx/productos/"+str(d['productId'])
+            # yield scrapy.Request(url=sale_flag_url, callback=self.parse_product, meta={'product': product})
+
+    # def parse_product(self, response):
+    #     product = response.meta.get('product')
+    #     d = product
+    #     api_url = "https://tienda.elflorido.com.mx:4433/Search/GetVariants?"+urlencode({'productId': d['productId'], 'siteId': 136})
+    #     print(api_url)
+    #     yield scrapy.Request(url=api_url, callback=self.parse_sale_flag, meta = {'products': product})
+    
 
 
+    # def parse_sale_flag(self, response):
+    #     raw_data = response.body
+    #     data = json.loads(raw_data)
+    #     print(data)
+    #     # sale_flag = f"Lleva {data['stock']} o más por ${data['unitPrice']}/PIEZA"
+    #     # product = response.meta.get('product')
+
+    #     # product['SaleFlag'] = sale_flag
+
+    #     # yield product
+
+    #     # api_url = "https://tienda.elflorido.com.mx:4433/Search/GetVariants?"+urlencode({'productId': d['productId'], 'siteId': d['siteId'] })
+    #     # api_url = "https://tienda.elflorido.com.mx:4433/Search/GetVariants?productId=13643&siteId=136"
 
         
 
